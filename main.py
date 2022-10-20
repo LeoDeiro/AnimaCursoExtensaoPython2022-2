333d971d1d3a23b68a6c769c4172641ca19988f9
Snake <64919417+LeoDeiro@users.noreply.github.com>
2022-10-20 01:22:54 +0000

    Chamber

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #print("")
 print("Hello World!\n")
 
-palavra = "Olá"
+palavra = "Word"
 numero = 26
 
 print("Sua palavra: "+palavra)
@@ -12,3 +12,5 @@ print(f"Seu número: {numero}"+"\n")
 #Outras formas de converter int para "string"
 print("-Seu número: "+str(numero))
 print("-seu número: {}".format(numero))
+
+print("Seu número e palavras: "+palavra+f" e {numero}")
