cc553cdd12fea268848f26e935d4b46e12454ec7
Snake <64919417+LeoDeiro@users.noreply.github.com>
2022-10-20 01:12:04 +0000

    Print

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ palavra = "Olá"
 numero = 26
 
 print("Sua palavra: "+palavra)
-print("Seu número: "+str(numero)+"\n")
+print(f"Seu número: {numero}"+"\n")
 
+#Outras formas de converter int para "string"
+print("-Seu número: "+str(numero))
+print("-seu número: {}".format(numero))
