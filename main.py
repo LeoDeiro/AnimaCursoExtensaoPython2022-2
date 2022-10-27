b14cd1298db53107ce2f80819ab7349b53d1a08b
Snake <64919417+LeoDeiro@users.noreply.github.com>
2022-10-27 00:54:14 +0000

    Aula 2

---
+++ main.py
@@ -0,0 +1,16 @@
+# Pedir o nome do aluna e a nota
+
+nome = input("Insira seu nome: ")
+nota = float(input("Digite sua nota: "))
+
+if(nota == 10):
+  print("Parabéns {}!".format(nome))
+elif(nota < 10 and nota >= 7):
+  print("Você passou!")
+elif(nota == 6):
+  print("Você passou na média.")
+elif(nota < 6 and nota >= 0):
+  print("Você reprovou. :(")
+elif(nota > 10 or nota < 0):
+  print("Insira um valor entre 0 e 10.")
+  
