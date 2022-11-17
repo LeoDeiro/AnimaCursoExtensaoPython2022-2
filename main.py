f5c114e1460d6e3e23c3e98001650afd2a2e01f5
Snake <64919417+LeoDeiro@users.noreply.github.com>
2022-11-17 00:24:42 +0000

    Aula 4

--- main.py
+++ main.py
@@ -9,18 +9,12 @@ conexao = sqlite3.connect("dc_universe.db")
 #Criar um cursor:
 cursor = conexao.cursor()
 
-#Comando SQL do banco:
-sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+#Inserir algo no arquivo:
+sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')"
 
-#Executar o comando SQL no SQLlite:
-cursor.execute(sql)
+#Executar o comando SQL:
+print(cursor.execute(sql))
 
-#Exibir a consulta:
-pessoas = cursor.fetchall()
-for pessoa in pessoas:
-  print(pessoa)
-
-print(45*("-"))
-
-for pessoa in pessoas:
-  print("Nome: {} ({})".format(pessoa[1],pessoa[3]))
+#Confirmar o INSERT:
+conexao.commit()
+conexao.close()
